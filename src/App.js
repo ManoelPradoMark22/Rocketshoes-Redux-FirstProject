77d77348c43d0078a28145bcbcfb77d0c35c2001
Manoel de Oliveira Prado Neto <manoelprado.aecjr@gmail.com>
2019-10-16 13:05:20 -0300

    stockOnAddition, ReactToastify

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux'; /* deixa o store da app disponivel
 para todos os componentes */
+import { ToastContainer } from 'react-toastify';
 
 import './config/ReactotronConfig'; // deve vir antes do import do store
 
@@ -18,6 +19,7 @@ function App() {
         <Header />
         <Routes />
         <GlobalStyle />
+        <ToastContainer autoClose={3000} />
       </BrowserRouter>
     </Provider>
   );
