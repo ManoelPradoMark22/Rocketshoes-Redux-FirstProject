2bbc84400af824176361088f420ec4bd4e5f8fb4
Manoel de Oliveira Prado Neto <manoelprado.aecjr@gmail.com>
2019-10-16 12:44:25 -0300

    separatingActions

--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -4,17 +4,9 @@ export default function cart(state = [], action) {
   switch (action.type) {
     case '@cart/ADD_SUCCESS':
       return produce(state, draft => {
-        const productIndex = draft.findIndex(p => p.id === action.product.id);
+        const { product } = action;
 
-        if (productIndex >= 0) {
-          draft[productIndex].amount += 1; // adiciona 1 na qnt
-        } else {
-          draft.push({
-            // cria o produto no carrinho
-            ...action.product,
-            amount: 1,
-          });
-        }
+        draft.push(product);
       });
     case '@cart/REMOVE':
       return produce(state, draft => {
--- src/store/modules/cart/sagas.js
+++ src/store/modules/cart/sagas.js
@@ -1,13 +1,15 @@
 import {
   call,
+  select /* busca informacoes no estado */,
   put,
   all,
   takeLatest,
 } from 'redux-saga/effects'; /* put dispara uma action */
 
 import api from '../../../services/api';
+import { fomatPrice, formatPrice } from '../../../util/format';
 
-import { addToCartSuccess } from './actions';
+import { addToCartSuccess, updateAmount } from './actions';
 
 /* o * é um generator. É um similar do async! Mas o generator
 tem mais funcionalidades que o async ! por isso vamos utilizá-lo. Mais adiante,
@@ -15,14 +17,33 @@ neste arquivo, vai aparecer o similar do await */
 /* Essa fç vai acessar a API, buscar as infos detalhadas do produto e
 cadastrar dentro do carrinho */
 function* addToCart({ id }) {
-  /* yield é o similar do await
-  atenção, no reduxSaga nao podemos fazer o api.get() diretamente, precisamos
-  fazer uns métodos auxiliares antes, como o call(), q é responsavel por
-  chamar métodos q são assincronos e q retornam promisses dentro do JS
-  Atenção: separe os parâmetros por vírgulas, pode passar varios param */
-  const response = yield call(api.get, `/products/${id}`);
+  /* procurar um produto no estado em que o id seja igual ao id do produto q
+  estamos tentnado adicionar */
+  const productExists = yield select(state =>
+    state.cart.find(p => p.id === id)
+  );
 
-  yield put(addToCartSuccess(response.data));
+  if (productExists) {
+    // se cair aqui, altera somente o amount
+    const amount = productExists.amount + 1;
+    yield put(updateAmount(id, amount));
+  } else {
+    // caso contrário, adiciona no carrinho
+    /* yield é o similar do await
+    atenção, no reduxSaga nao podemos fazer o api.get() diretamente, precisamos
+    fazer uns métodos auxiliares antes, como o call(), q é responsavel por
+    chamar métodos q são assincronos e q retornam promisses dentro do JS
+    Atenção: separe os parâmetros por vírgulas, pode passar varios param */
+    const response = yield call(api.get, `/products/${id}`);
+
+    const data = {
+      ...response.data,
+      amount: 1,
+      priceFormatted: formatPrice(response.data.price),
+    };
+
+    yield put(addToCartSuccess(data));
+  }
 }
 
 /* all é para cadastrar vários listeners
